8e6bb26c740017e40b45ea82b7c7d919b7bfd479
Emanuele <emanuelemastaglia@gmail.com>
2022-01-31 16:37:44 +0100

    test before loop

--- src/Game/PlayerController.js
+++ src/Game/PlayerController.js
@@ -4,13 +4,18 @@ import {Color3, MeshBuilder, StandardMaterial} from "@babylonjs/core";
 class PlayerController extends GameObject{
     constructor() {
         super();
-        this.player= MeshBuilder.CreateSphere("player",{segments:8,diameter:1.8}, this.scene);
+        this.player= MeshBuilder.CreateBox("player",{width:8,height:8}, this.scene);
         this.player.material= new StandardMaterial("player_mat",this.scene);
         this.player.material.emissiveColor= Color3.FromHexString('#ff9900')
         this.player.position.y=0.9
         this.player.checkCollisions= true;
-    }
 
+    }
+   remove() {
+        const deltaTimeInMillis = this.scene.getEngine().getDeltaTime();
+        const rpm = 100;
+        this.player.rotation.y +=(rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000); //     if (box !== undefined) {
+    }
 }
 
 export default PlayerController
